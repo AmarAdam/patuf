e94cf80aa25088db64a47e9dabe7651101918e3b
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2018-06-03 15:33:59 +0200

    Update list_team.php

--- application/views/pages/team/list_team.php
+++ application/views/pages/team/list_team.php
@@ -1,15 +1,16 @@
-      <div class="container" class="col-xs-8 col-md-6 col-md-offset-6 col-sm-8 col-sm-offset-6">  
-        <div class="content mt-3">
+      <div class="container">  
+            <div class="container">
+            <div class="content mt-3">
                 <div class="animated fadeIn">
-                  <div>
+
                   <div class="row">
-                      <div>
-                          <div class="box" >
-                              <strong>Liste des Users</strong>
+                      <div class="col-md-12">
+                          <div class="box">
+                              <strong>Liste des Equipes</strong>
                           
                             <table class="table table-striped">
                             <thead>
-                                <tr>
+                                 <tr>
                                     <th scope="col">Nom de l'équipe</th>
                                     <th scope="col">idTeam</th>
                                     <th scope="col">Nombre de joueurs</th>
@@ -32,25 +33,39 @@
                               </table>
                             </div>
                           </div>
-                          </div>
                         </div>
+                      </div>
+                    </div>
+                  </div>
 
-                       <p> Voulez vous supprimer une équipe ?</p>
+                    <br></br>
+                    <br></br>
+                    <br></br>
 
+                    <div class="container">
                           <?php echo form_open('TeamCtrl/deleteTeam'); ?>
 
                             <form class="form-inline"  method="post">
-                              
+                            
+                            <div class="text-center">                    
+                            <h2 class="page-header" class="text-center">Suppression des Joueurs :</h2></div></div>
+                            <br></br>
+
                               <div class="form-group">
-                                <label class="control-label">Id</label>
+                                <label class="control-label">Entrez ici l'identifiant du joueur à supprimer :</label>
                                   <input type="number" class="form-control" name="idteam" value="<?php echo set_value('idteam'); ?>" size="30" required/> </div>
-                              <div class="text-center"><input class="btn btn-primary btn-success btn-block" type="submit" value="Supression" size="15" /></div>
+                              <div class="text-center"><input class="btn btn-primary btn-success btn-block" type="submit" value="Supression" size="5" /></div>
                             </form>
                         </div>
                       </div>
                     </div>
+                  </div>
 
-                    <div class="strong" class="text-center"> Voulez vous modifier une équipe ?</div>
+                    <br></br>
+
+                    <br></br>
+
+                       
 
                     <div class="container">
 
@@ -82,13 +97,15 @@
                               
                               <div class="text-center"><input class="btn btn-primary btn-success btn-block" type="submit" value=" Inscription " /></div>
                               <div class="text-center">
-                                  <br>
+                                  <br></br>
+                                  <br></br>
+
                                   <h1 style="color:darkslategrey; " >Modifiez ici, les Equipes composants sur le Site PATUF ! </h1>
                               </div>
                             </form>
                           </div>
-                        
-
+                          <br></br>
+                          <br></br>
 
                         </div>
                       </div> 
