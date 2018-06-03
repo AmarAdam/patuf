3c921ed2be2b95e6fc2597a4dd5b054544c3c81a
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2018-06-03 19:21:06 +0200

    Update TypeCtrl.php

--- application/controllers/TypeCtrl.php
+++ application/controllers/TypeCtrl.php
@@ -19,6 +19,36 @@
 			$this->load->view('pages/type/list_type',$data);
 			$this->load->view('templates/footer');
 		}
+
+		public function addtype(){
+
+            if (isset($_COOKIE['login']) && $this->My_admin->verifcookie($_COOKIE['login'])) {
+
+            $this->load->helper('form');
+            $this->load->view('templates/header');
+            $this->load->view('pages/type/add_type');}
+
+            else{
+                $this->load->view('pages/PageConnexion');
+                }
+        }
+
+		public function createType(){
+			$this->load->model('my_type');
+			$this -> load -> library('form_validation');
+
+			if (isset($_COOKIE['login']) && $this->My_admin->verifcookie($_COOKIE['login'])) {
+				$data= htmlspecialchars($_POST['nomtype']);
+   				$this->my_type->insert($data);
+
+				$this->load->view('templates/header');
+				}
+			
+			else{
+				$this->load->view('pages/PageConnexion');
+				}
+
+			}
 	
 
 
