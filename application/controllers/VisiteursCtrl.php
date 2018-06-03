c841cf7b94d3c871b92e9d44eb6537aa8fc287e5
AmarAdam <adammegherat@gmail.com>
2018-06-03 03:05:21 +0200

    non

--- application/controllers/VisiteursCtrl.php
+++ application/controllers/VisiteursCtrl.php
@@ -4,18 +4,11 @@
 
 	class VisiteursCtrl extends CI_Controller{
 		
-		public function accueil($page = 'PageAccueil'){
-		
-			if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
-				show_404();
-			}
-
+		public function accueil(){
+			
 			$this->load->database();
-
-			$data['title'] = ucfirst($page);
 			
 			$this->load->view('templates/headervisiteurs');
-			$this->load->view('pages/'.$page, $data);
 			$this->load->view('templates/footer');
 
 		}
