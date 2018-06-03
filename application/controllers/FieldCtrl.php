01d0233d563302f0c92aa2da06c0739645334e01
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2018-06-03 19:12:19 +0200

    Update FieldCtrl.php

--- application/controllers/FieldCtrl.php
+++ application/controllers/FieldCtrl.php
@@ -11,6 +11,41 @@
 			}
 		}
 
+		public function addfield(){
+
+			if (isset($_COOKIE['login']) && $this->My_admin->verifcookie($_COOKIE['login'])) {
+
+  			$this->load->helper('form');
+			$this->load->view('templates/header');
+			$this->load->view('pages/field/add_field');}
+
+			else{
+				$this->load->view('pages/PageConnexion');
+				}
+		}
+
+		public function createField(){
+			$this->load->model('my_field');
+			$this -> load -> library('form_validation');
+
+			if (isset($_COOKIE['login']) && $this->My_admin->verifcookie($_COOKIE['login'])) {
+				
+				$data=array(
+            	"namefield"=> htmlspecialchars($_POST['namefield']),
+            	"adressefield"=> htmlspecialchars($_POST['adressefield']),
+				);
+   				$this->my_field->insert($data);
+
+				$this->load->view('templates/header');
+				}
+			
+			else{
+				$this->load->view('pages/PageConnexion');
+				}
+
+			}
+
+
 
 		public function listfield(){
 			$this->load->model('my_field');
