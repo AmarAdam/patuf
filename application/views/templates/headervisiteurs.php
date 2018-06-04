830b7119640808f268401c521135fc8a5f1d69e0
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2018-06-04 10:39:51 +0200

    Update headervisiteurs.php

--- application/views/templates/headervisiteurs.php
+++ application/views/templates/headervisiteurs.php
@@ -26,9 +26,7 @@
         <div id="sidebar-wrapper">
             <ul class="sidebar-nav">
                 <li class="sidebar-brand">
-                    <a href="#">
-                        Start Bootstrap
-                    </a>
+                
                 </li>
                 <li>
                     <a href="<?php echo site_url()?>/">Accueil</a>
