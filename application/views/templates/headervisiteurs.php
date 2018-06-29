566be6f6fc73804b67512f9476c319432b1b8e29
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2018-06-29 14:30:50 +0100

    Update headervisiteurs.php

--- application/views/templates/headervisiteurs.php
+++ application/views/templates/headervisiteurs.php
@@ -53,11 +53,7 @@
 				<li>
                     <a href="<?php echo base_url()?>/ContactCtrl/contact">Contact</a>
                 </li>
-               
-                
-                <li>
-                    <a href="#">Contact</a>
-                </li>
+              
             </ul>
         </div>
         <!-- /#sidebar-wrapper -->
