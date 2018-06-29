d2472905e6d54cf35d132c3acf683c913eeac7ca
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2018-06-29 14:19:37 +0100

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,21 @@
-		</div>
-	</body>
+ </div>
+    <!-- /#wrapper -->
 
+    <!-- Bootstrap core JavaScript -->
+    <script src="<?php echo base_url().'/assets/vendor/jquery/jquery.min.js';?>"></script>
+    <script src="<?php echo base_url().'/assets/vendor/bootstrap/js/bootstrap.bundle.min.js';?>"></script>
+
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+
+</body>
+	
 	<p>Appli Web by Adam</p>
 
 </html>
+
