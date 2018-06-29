456157b8eb6c7d7657d2182c881eaf87b73f858b
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2018-06-29 13:38:45 +0100

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -67,6 +67,9 @@
                 <li>
                     <a href="<?php echo base_url()?>/DeconnexionCtrl/deconnexion">Déconnexion</a>
                 </li>
+				<li>
+                    <a href="<?php echo base_url()?>/ContactCtrl/contact">Contact</a>
+                </li>
             </ul>
         </div>
         <!-- /#sidebar-wrapper -->
