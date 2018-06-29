fbf3772027e4f8da41cd39281d442e4944b16333
AmarAdam <32563426+AmarAdam@users.noreply.github.com>
2018-06-29 13:41:43 +0100

    Create contact.php

---
+++ application/views/pages/contact.php
@@ -0,0 +1,7 @@
+<div class="container">
+
+	<h2>CONTACT :</h2>
+
+	<p>Vous pouvez contacter l'administrateur de l'appli Web PATUF par mail : adammegherat@gmail.com, ou par téléphone, au 06 95 85 75 09.</p>
+
+</div>
